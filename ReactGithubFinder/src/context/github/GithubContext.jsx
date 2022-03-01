134220c06cbf46fa5adec9821627838cae37be50
Ricky Aston <astonmedia@hotmail.com>
2022-03-01 20:49:40 +1000

    completed reducers

--- ReactGithubFinder/src/context/github/GithubContext.jsx
+++ ReactGithubFinder/src/context/github/GithubContext.jsx
@@ -1,28 +1,47 @@
-import { createContext, useState } from "react"
+import { createContext, useReducer } from "react"
+import githubReducer from "./GithubReducer"
 
 const GithubContext = createContext()
 
 const GITHUB_URL = import.meta.env.VITE_GITHUB_URL
-const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN
 
 export const GithubProvider = ({ children }) => {
-  const [users, setUsers] = useState([])
-  const [loading, setLoading] = useState(true)
-
-  const fetchUsers = async () => {
-    const response = await fetch(`${GITHUB_URL}/users`, {
-      // headers: {
-      //   Authorization: `token ${import.meta.env.VITE_GITHUB_TOKEN}`,
-      // },
-    })
-    const data = await response.json()
-
-    setUsers(data)
-    setLoading(false)
+  const initialState = {
+    users: [],
+    loading: false,
   }
 
+  const [state, dispatch] = useReducer(githubReducer, initialState)
+
+  // Get search results
+  const searchUsers = async (text) => {
+    setLoading()
+    const params = new URLSearchParams({
+      q: text,
+    })
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`)
+    const { items } = await response.json()
+
+    dispatch({
+      type: "GET_USERS",
+      payload: items,
+    })
+  }
+  // Clear users from state
+  const clearUsers = () => dispatch({ type: "CLEAR_USERS" })
+
+  // Set Loading
+  const setLoading = () => dispatch({ type: "SET_LOADING" })
+
   return (
-    <GithubContext.Provider value={{ users, loading, fetchUsers }}>
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        loading: state.loading,
+        searchUsers,
+        clearUsers,
+      }}
+    >
       {children}
     </GithubContext.Provider>
   )
